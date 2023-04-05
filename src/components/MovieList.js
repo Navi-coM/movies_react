be1052626689dcc7d90c2fc554a234a1d1368540
Ivan Vasilenko <ivan.vasilenko@ys.bet>
2023-04-05 15:56:39 +0300

    preparing and uploading a project for git

---
+++ src/components/MovieList.js
@@ -0,0 +1,13 @@
+import MovieItem from './MovieItem'
+
+
+function MovieList(props) {
+    const {movieList} = props
+
+   return <section className="movies">
+        {movieList.map(movie => <MovieItem key={movie.imdbID} {...movie} />)}
+   </section>
+
+}
+
+export default MovieList;
---
+++ src/layout/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+    return <nav className="indigo darken-4">
+        <div className="nav-wrapper container">
+            <a href="#" className="brand-logo">Movies By Ivan</a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><a href="#">Movie List</a></li>
+            </ul>
+        </div>
+    </nav>
+}
+
+export default Header;
